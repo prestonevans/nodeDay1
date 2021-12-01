c650f22f101f767058ba303f28e60b551f643e24
Preston Evans <preston.evans3074@stu.mtec.edu>
2021-12-01 10:36:35 -0700

    have client plant game sending input to server and back to client

--- plantClient.js
+++ plantClient.js
@@ -1,14 +1,19 @@
 const net = require('net'); // Client Code  .
+const input = process.stdin;
 const client = net.createConnection({ port: 3000 }, () => {
-	// once connected .
+    // once connected .
 	console.log('connected to server!');
 	client.write('1 ... Anyone there!\r\n');
 	client.write('2 ... What did you say!\r');
 });
+
 client.on('data', (data) => {
-	console.log('Msg from server:' + data.toString());
-	client.end();
+    console.log('Msg from server:' + data.toString());
+	// client.end();
 });
+input.on('data', (data) => {
+    client.write(data.toString());
+})
 client.on('end', () => {
 	console.log('End of Message');
 });
